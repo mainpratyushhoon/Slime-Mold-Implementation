558d1cf9717ce57d4173c2645929f2876847a5c6
mainpratyushhoon <mainpratyushoon@gmail.com>
2025-08-23 20:44:25 +0530

    Food added

--- SlimeMain.cpp
+++ SlimeMain.cpp
@@ -3,18 +3,24 @@
 #include <cmath>
 #include <random>
 #include <iostream>
+#include <time.h>
 using namespace std;
 const int WIN_W = 800;
 const int WIN_H = 800;
-const int NUM_AGENTS = 1000;
+const int NUM_AGENTS = 50000;
+const int NUM_POINTS = 10;
 
 struct Agent {
     float x, y;
     float angle;
 };
 
+struct Point {
+    float x,y;
+};
 
 vector<Agent> agents;
+vector<Point> points(NUM_POINTS);
 vector<float> trail;  // 2D trail map
 int GRID_W = 150, GRID_H = 150;
 
@@ -24,12 +30,16 @@ uniform_real_distribution<float> uni01(0.0f,1.0f);
 // parameters
 float sensor_distance = 3.0f;
 float sensor_angle = 0.5f;
-float turn_angle = 0.01f;
+float turn_angle = 0.1f;
 float step_size = 0.1f;
 float deposit_amount = 1.0f;
-float evaporation = 0.01f;
+float evaporation = 0.1f;
+
 
 inline int idx(int x,int y){ return y*GRID_W + x; }
+int randomINT(){
+    return rand()%GRID_H;
+}
 
 void initAgents(){
     agents.resize(NUM_AGENTS);
@@ -47,12 +57,18 @@ float sampleTrail(float x,float y){
     return trail[idx(xi,yi)];
 }
 
-void deposit(float x,float y){
+void deposit(float x,float y,float amount = deposit_amount){
     int xi=(int)x; int yi=(int)y;
     if(xi<0||xi>=GRID_W||yi<0||yi>=GRID_H) return;
-    trail[idx(xi,yi)] += deposit_amount;
+    trail[idx(xi,yi)] += amount;
 }
-
+void assignPoints(){
+    for(int i=0;i<NUM_POINTS;i++){
+        points[i].x=randomINT();
+        points[i].y=randomINT();
+        deposit(points[i].x, points[i].y,1000.0);
+    }
+}
 void updateAgents(){
     for(auto &a:agents){
         float ax=a.x+cos(a.angle)*sensor_distance;
@@ -97,7 +113,11 @@ void updateAgents(){
     }
 
         deposit(a.x,a.y);
+        
     }
+    for(auto itr: points){
+            deposit(itr.x,itr.y,100.0);
+        }
 }
 
 void evaporate(){
@@ -117,16 +137,20 @@ void display(){
     evaporate();
 
     glBegin(GL_POINTS);
+
     for(int y=0;y<GRID_H;y++){
         for(int x=0;x<GRID_W;x++){
             float v=trail[idx(x,y)];
             if(v>0.01f){
                 float c = std::min(1.0f,v*0.05f);
-                glColor3f(c,c,c);
+                if(v>1000.0) glColor3f(1,0,0);
+                else glColor3f(c,c,c);
                 glVertex2f((float)x/GRID_W*2-1, (float)y/GRID_H*2-1);
             }
         }
     }
+
+    
     glEnd();
     
     glutSwapBuffers();
@@ -134,6 +158,7 @@ void display(){
 }
 
 int main(int argc,char**argv){
+    srand(0);
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
     glutInitWindowSize(WIN_W,WIN_H);
@@ -141,10 +166,10 @@ int main(int argc,char**argv){
 
     glClearColor(0,0,0,1);
     glPointSize(2.0f);
-    glEnable(GL_POINT_SMOOTH);
+    //glEnable(GL_POINT_SMOOTH);
 
     initAgents();
-
+    assignPoints();
     glutDisplayFunc(display);
     glutMainLoop();
     return 0;
